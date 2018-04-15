cc80ecae1c8964f8e27d55d0ca196c4b9f4ac46b
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2018-04-14 22:38:46 -0400

    Add config to be pushed indicator

--- .config/i3/i3blocks-commands/isConfigChanged.sh
+++ .config/i3/i3blocks-commands/isConfigChanged.sh
@@ -4,6 +4,8 @@ if [[ `git -C ~ fetch --dry-run` ]]; then
     echo "<span color='#ff5ef4'>NEW REMOTE</span>"
 elif [[ `git -C ~ status --porcelain` ]]; then
     echo "<span color='#ff5ef4'>NEW LOCAL</span>"
+elif [ "$(git rev-parse master)" != "$(git rev-parse origin/master)" ]; then
+    echo "<span color='#ffff00'>TO BE PUSHED</span>"
 else
    echo "OK"
 fi
